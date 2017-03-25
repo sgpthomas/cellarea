efa65da87cdf94304659f1c9644b51d6029f28ee
Rachel Susanne Nash <rsn55@cornell.edu>
2017-03-25 16:43:32 -0400

    sup bruh

--- code/area.py
+++ code/area.py
@@ -3,6 +3,19 @@ import numpy
 import scipy
 from scipy import ndimage
 
+"""steps:
+1. convert to grayscale
+2. run Gaussian blur - average nearby pixels
+3. run sobel edge detection
+4. run canny edge detector 
+5. detect what edges make up a cell
+6. calculate area of cells for image
+7. set it up so that all images in a file are processed and
+data is printed out labelled for each image
+8. make most efficient/accurate as possible
+9. possibly do some kind of standard deviation statistics
+"""
+
 im = Image.open("../images/DSC_0625.jpeg") # open colour image
 
 im = im.convert('L') # convert image to grayscale
